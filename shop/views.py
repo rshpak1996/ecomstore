ab0a58252acf96c7557be18eb810ed888f77c774
Roman Shpak <58186540+rshpak1996@users.noreply.github.com>
2020-11-04 13:43:19 +0200

    Add about page and modify home page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-    return render (request, 'shop/home.html')
+    return render(request, 'shop/home.html')
+
+
+def about(request):
+    return render(request, 'shop/about.html')

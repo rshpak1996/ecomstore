4f46b07eb50e661cb45f8b63107cc0cc446f3db0
Roman Shpak <58186540+rshpak1996@users.noreply.github.com>
2020-11-04 13:14:58 +0200

    Connected the postgresql

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecomstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecomstoredb',
+        'USER': 'postgres',
+        'PASSWORD': '1337',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

ac8912113210663c81ca62f5313d0998c0aefcc6
Roman Shpak <58186540+rshpak1996@users.noreply.github.com>
2020-11-06 16:54:59 +0200

    Displaying products on the home page

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, unique="True")
-    slug = models.SlugField(max_length=250, unique="True")
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to="category", blank="True")
+    image = models.ImageField(upload_to="category", blank=True)
 
     class Meta:
         ordering = ('name', )
@@ -20,12 +20,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, unique="True")
-    slug = models.SlugField(max_length=250, unique="True")
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="product", blank="True")
+    image = models.ImageField(upload_to="product", blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)

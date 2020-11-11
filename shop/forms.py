ff09190cd04b428e096ea114b236e7f8b895b965
Roman Shpak <rshpak1996@gmail.com>
2020-11-11 21:46:13 +0200

    User registration form

---
+++ shop/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=100, help_text='r.shpak1996@gmail.com')
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name',
+                  'username', 'password1', 'password2')
